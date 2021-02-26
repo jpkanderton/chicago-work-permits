47a51eb65c6f10d1e3ea078d14920c6554e753db
John Anderton <janderton24@gmail.com>
2021-02-26 14:59:30 -0700

    adds conditional to show either Dashboard  or Pending .jsx

--- client/src/components/App.jsx
+++ client/src/components/App.jsx
@@ -4,8 +4,9 @@ import Dashboard from './Dashboard.jsx';
 import Header from './Header.jsx';
 import Pending from './Loading.jsx';
 
+const { useState, useEffect } = React;
+
 const App = () =>{
-  const { useState, useEffect } = React;
   const [data, setData] = useState([]);
   const [currentPermit, setCurrentPermit] = useState({});
 
@@ -27,6 +28,7 @@ const App = () =>{
   return (
   <div id="app-container">
     <Header />
+    {data.length > 0 ? <Dashboard data = {data} currentPermit = {currentPermit} clickEvent= {clickEvent} /> : <Pending />}
   </div>)
 }
 
