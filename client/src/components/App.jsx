b51f1dd65fff2e50758df91a175cf0e5e9c55a37
John Anderton <janderton24@gmail.com>
2021-02-26 13:58:48 -0700

    adds first App component with skeleton structure for React Hooks

---
+++ client/src/components/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import axios from 'axios';
+
+const { useState, useEffect } = React;
+
+const App = () =>{
+  return (
+  <div id="app-container">
+    Hello World
+  </div>)
+}
+
+export default App;
