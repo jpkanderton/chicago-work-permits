122bbaeb4f7eb5cad60ffc6cd0e53068169bb626
John Anderton <janderton24@gmail.com>
2021-02-26 13:39:53 -0700

    adds express, body-parser, and axios to app.js in order to serve static file

---
+++ server/app.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const axios = require('axios');
+
+const app = express();
+
+app.use(express.static('./client/dist'));
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.json());
+
+app.get('/', (req, res) => {
+  res.send('hello world');
+})
+
+module.exports = app;
